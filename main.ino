e1aecf4113a35c240093675077f2de39988164a0
Cyrus Studying <cyrustudying@gmail.com>
2025-06-06 16:26:23 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,12 +1,19 @@
 int profundidade;
+float ml;
 bool start;
 
 void setup(){
   profundidade = 0;
   start = true;
+  ml = 0;
 
+  
   Serial.print("Começou com profundidade de: ");
   Serial.println(profundidade);
+
+
+  Serial.print("Começou com quantidade de ml =  ");
+  Serial.println(ml);
 }
 
 
