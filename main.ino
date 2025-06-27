43a32a76de9d78aaed1725a666a3f2c165759861
Cyrus Studying <cyrustudying@gmail.com>
2025-06-27 16:50:05 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,26 +1,43 @@
+// Variáveis
 int profundidade;
 float ml;
 bool start;
 int todosOsMl;
 int cmParaMl;
 
+
 void setup(){
+  // Definindo as variáveis
   profundidade = 0;
   start = true;
   ml = 0;
   todosOsMl = 10000;
-  cmParaMl = 
+  cmParaMl = 400;
 
-  
+
+  // Debug profundidade
   Serial.print("Começou com profundidade de: ");
   Serial.println(profundidade);
 
 
+  // Debug ml
   Serial.print("Começou com quantidade de ml =  ");
   Serial.println(ml);
 }
 
 
 void loop(){
-  
+  // Calculando ml
+  // função()
+  // profundidade = var-resulante
+  ml = profundidade * cmParaMl;
+
+
+  // Display ml
+  // função(ml);
+
+
+  // Debug loop
+  Serial.println(profundidade);
+  Serial.println(ml);
 }
